f065d29ec3a90a94d69946fece4c9ac3be1c7b0b
fabricio <fnmamao@gmail.com>
2020-05-15 16:00:23 -0300

    exercicio

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string digitado;
+           Console.WriteLine("digite algo...");
+           digitado = Console.ReadLine();
+           Console.WriteLine(digitado.ToUpper());
+        }
+    }
+}
